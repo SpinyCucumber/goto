6cc8cea0522ec1b92338a842b860c3fc2fea034e
Elijah Hilty <elijah.hilty@hexagon.com>
2024-06-01 02:30:57 -0500

    Display tags, hover

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -6,23 +6,27 @@ import { computed, ref, type Ref } from 'vue';
 interface Link {
   name: string;
   uri: string;
+  tags: string[];
 }
 
 // TODO Retrieve from URL
 const search = ref("");
 const links: Ref<Link[]> = ref([
-  { name: "Wikipedia", uri: "https://en.wikipedia.org" },
-  { name: "The Crimson White", uri: "https://thecrimsonwhite.com/" }
+  { name: "Wikipedia", uri: "https://en.wikipedia.org", tags: ["wiki"] },
+  { name: "The Crimson White", uri: "https://thecrimsonwhite.com/", tags: ["news", "bama"] }
 ]);
 
 // We compute a search index from list of links
 const suffixArray = computed(() => {
   const builder = new SuffixArrayBuilder<Link>();
   for (const link of links.value) {
-    // Use name, URI host, and URI path as labels
+    // Use name, URI host, URI path, and tags as labels
     const uri = new URL(link.uri);
     builder.addLabel(link, link.name);
     builder.addLabel(link, uri.host);
+    for (const tag of link.tags) {
+      builder.addLabel(link, tag);
+    }
   }
   return builder.build();
 });
@@ -32,14 +36,19 @@ const searchResults = computed(() => {
   return suffixArray.value.search(search.value);
 })
 
+function activate(link: Link) {
+  window.open(link.uri);
+}
+
 </script>
 
 <template>
   <main>
     <CustomInput class="search-input" v-model="search"/>
     <ul class="results">
-      <li v-for="result in searchResults">
+      <li v-for="result in searchResults" @click="activate(result)">
         <span class="result-name">{{ result.name }}</span>
+        <span class="result-tags">({{ result.tags.join(",") }})</span>
       </li>
     </ul>
   </main>
@@ -64,14 +73,39 @@ main {
 }
 
 .results > li {
-  padding: 0.35rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  padding-top: 0.4rem;
+  padding-bottom: 0.4rem;
+  padding-left: 0.4rem;
+  transition: background-color 0.25s;
 }
 
 .results > li + li {
-  border-top: 1px solid color-mix(in srgb, var(--color-border) 25%, transparent)
+  border-top: 1px solid color-mix(in srgb, var(--color-border) 25%, transparent);
+}
+
+.results > li:hover {
+  background-color: var(--color-link-hover);
+}
+
+.results > li:hover .result-name {
+  color: var(--color-link);
+}
+
+.results > li:hover .result-tags {
+  color: color-mix(in srgb, var(--color-link) 25%, transparent);
 }
 
 .result-name {
-  font-size: 20px;
+  font-size: 18px;
+  transition: color 0.25s;
+}
+
+.result-tags {
+  font-size: 16px;
+  color: color-mix(in srgb, var(--color-text) 25%, transparent);
+  transition: color 0.25s;
 }
 </style>
